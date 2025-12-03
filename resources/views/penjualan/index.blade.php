4136bde48dd2ae61565173d5178e7d618436298e
Faizpi <faizbintang1244@gmail.com>
2025-12-03 14:16:08 +0700

    fix: Modal cancel centered seperti modal hapus

--- resources/views/penjualan/index.blade.php
+++ resources/views/penjualan/index.blade.php
@@ -194,7 +194,7 @@
     </div>
 
     <div class="modal fade" id="cancelModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header bg-warning">
                     <h5 class="modal-title"><i class="fas fa-exclamation-triangle mr-2"></i>Konfirmasi Pembatalan</h5>
